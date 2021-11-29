caccdbcacbd8afba0d032b990a07c2ea63869418
curder <q.curder@gmail.com>
2021-11-29 13:29:10 +0800

    Add TouchableOpacity component

--- App.tsx
+++ App.tsx
@@ -1,29 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {Alert, Button, StyleSheet, Text, View} from 'react-native';
+import {Alert, Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={{ color: 'red' }}>Open up App.tsx to start working on your app now!</Text>
-      <StatusBar style="auto" />
+    const onPress = () => {
+        Alert.alert('Touchable opacity pressed')
+    }
 
-      <Button
-          title="Press me"
-          color='green'
-          onPress={() => Alert.alert('Simple Button pressed')}
-      />
+    return (
+        <View style={styles.container}>
+            <Text style={{color: 'red'}}>Open up App.tsx to start working on your app now!</Text>
+            <StatusBar style="auto"/>
 
-    </View>
-  );
+            <Button
+                title="Press me"
+                color='green'
+                onPress={() => Alert.alert('Simple Button pressed')}
+            />
+            <TouchableOpacity
+                style={styles.button}
+                onPress={onPress}
+            >
+                <Text>Press Here</Text>
+            </TouchableOpacity>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 60,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    // justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        marginTop: 60,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    button: {
+        alignItems: "center",
+        flexDirection: 'row',
+        backgroundColor: "#ddd",
+        padding: 10
+    },
 });
