ac020e65da04f7423fada358d6478b75396fbfea
curder <q.curder@gmail.com>
2021-11-29 18:13:01 +0800

    Add tabbarIcon

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {Ionicons} from '@expo/vector-icons';
 
 function HomeScreen() {
     return (
@@ -24,9 +25,21 @@ const Tab = createBottomTabNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={HomeScreen}/>
-                <Tab.Screen name="Settings" component={SettingsScreen}/>
+            <Tab.Navigator screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName = "";
+
+                    if (route.name === 'Home') {
+                        iconName = "home"
+                    } else if (route.name === 'Settings') {
+                        iconName = "settings"
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color}/>;
+                },
+            })}>
+                <Tab.Screen name="Home" component={HomeScreen} options={{tabBarLabel: 'My Home'}}/>
+                <Tab.Screen name="Settings" component={SettingsScreen} options={{ tabBarLabel: 'Settings'}}/>
             </Tab.Navigator>
         </NavigationContainer>
     );
