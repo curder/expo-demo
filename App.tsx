d609480ada49fd26c3c1c28a6ead9cae7c84bbe7
curder <q.curder@gmail.com>
2021-11-29 13:27:37 +0800

    Add button component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert, Button, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{ color: 'red' }}>Open up App.tsx to start working on your app now!</Text>
       <StatusBar style="auto" />
+
+      <Button
+          title="Press me"
+          color='green'
+          onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
     </View>
   );
 }
