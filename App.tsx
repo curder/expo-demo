7ea20c028c3c331922d88ecf0d6d1411711fb1ff
curder <q.curder@gmail.com>
2021-11-29 16:44:46 +0800

    Add @react-navigation/native-stack package

--- App.tsx
+++ App.tsx
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, Text, View} from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer, useFocusEffect} from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function HomeScreen({navigation}) {
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => navigation.navigate('Details', {
+                    itemId: 86,
+                    otherParam: 'anything you want here',
+                })}
+            />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({route}) {
+    const {itemId, otherParam} = route.params;
+
+    useFocusEffect(
+        React.useCallback(() => {
+            console.log("Detail screen is focused");
+
+            return () => {
+                console.log("Detail screen is unfocused");
+            };
+        }, [])
+    );
+    return (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text>Detail Screen for {itemId} </Text>
+            <Text>{otherParam}</Text>
+        </View>
+    );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName={'Home'}>
+                <Stack.Screen name="Home" component={HomeScreen} options={{title: 'My Home'}}/>
+                <Stack.Screen name="Details" component={DetailsScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
