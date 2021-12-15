6c4d66828dbd2e3a9785e7482576db5d1b94230b
curder <q.curder@gmail.com>
2021-12-15 16:52:42 +0800

    Pull to refresh

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,60 +1,78 @@
-import React, {useEffect, useState} from "react";
-import {Text, View, StyleSheet, FlatList, Image, TouchableOpacity, Platform} from "react-native";
-import {AntDesign, EvilIcons} from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet, FlatList, Image, TouchableOpacity, Platform, ActivityIndicator } from "react-native";
+import { AntDesign, EvilIcons } from "@expo/vector-icons";
 import axios from "axios";
 import dayjs from 'dayjs';
 import '../helpers/formatCustomLocale.js';
 import relativeTime from 'dayjs/plugin/relativeTime';
+
 dayjs.extend(relativeTime)
 
-export default function HomeScreen ({ navigation }) {
+export default function HomeScreen({navigation}) {
   const [data, setData] = useState([]);
-  useEffect(()=> {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  useEffect(() => {
     getAllTweets()
   }, [])
 
   let getAllTweets = () => {
-    axios.get('https://8965-2409-8a00-6051-d080-24fa-719e-2c45-3295.ngrok.io/api/tweets')
-      .then(res => setData(res.data))
-      .catch(e => console.log(e));
+    axios.get('https://5e1c-222-128-100-27.ngrok.io/api/tweets')
+      .then(res => {
+        setData(res.data)
+        setIsLoading(false)
+        setIsRefreshing(false)
+      })
+      .catch(e => {
+        console.log(e)
+        setIsLoading(false)
+        setIsRefreshing(false)
+      });
   }
 
-  const renderItem = ({ item: tweet }) => (
-    <View style={ styles.tweetContainer }>
-      <TouchableOpacity onPress={ () => goToProfile() }>
-        <Image style={ styles.avatar } source={ { uri: tweet.user.avatar } }/>
+  let handleRefresh = () => {
+    setIsRefreshing(true)
+    getAllTweets();
+  }
+
+  const renderItem = ({item: tweet}) => (
+    <View style={styles.tweetContainer}>
+      <TouchableOpacity onPress={() => goToProfile()}>
+        <Image style={styles.avatar} source={{uri: tweet.user.avatar}}/>
       </TouchableOpacity>
-      <View style={ { flex: 1, } }>
-        <TouchableOpacity style={ styles.flexRow } onPress={ () => goToSignalTweet() }>
-          <Text numberOfLines={ 1 } style={ styles.tweetName }>{ tweet.user.name }</Text>
-          <Text numberOfLines={ 1 } style={ styles.tweetHandle }>@{tweet.user.username}</Text>
+      <View style={{flex: 1,}}>
+        <TouchableOpacity style={styles.flexRow} onPress={() => goToSignalTweet()}>
+          <Text numberOfLines={1} style={styles.tweetName}>{tweet.user.name}</Text>
+          <Text numberOfLines={1} style={styles.tweetHandle}>@{tweet.user.username}</Text>
           <Text>&middot;</Text>
-          <Text numberOfLines={ 1 } style={ styles.tweetHandle }>{dayjs(tweet.created_at).locale('custom-locale').toNow()}</Text>
+          <Text numberOfLines={1}
+                style={styles.tweetHandle}>{dayjs(tweet.created_at).locale('custom-locale').toNow()}</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={ styles.tweetContent } onPress={ () => goToSignalTweet() }>
-          <Text style={ styles.tweetContentContainer }>{ tweet.body }</Text>
+        <TouchableOpacity style={styles.tweetContent} onPress={() => goToSignalTweet()}>
+          <Text style={styles.tweetContentContainer}>{tweet.body}</Text>
         </TouchableOpacity>
 
-        <View style={ styles.tweetEngagement }>
-          <TouchableOpacity style={ styles.flexRow }>
-            <EvilIcons name={ 'comment' } size={ 22 } style={ { marginRight: 2 } } color={ "gray" }/>
-            <Text style={ styles.textGray }>5,456</Text>
+        <View style={styles.tweetEngagement}>
+          <TouchableOpacity style={styles.flexRow}>
+            <EvilIcons name={'comment'} size={22} style={{marginRight: 2}} color={"gray"}/>
+            <Text style={styles.textGray}>5,456</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={ [styles.flexRow, styles.ml16] }>
-            <EvilIcons name={ 'retweet' } size={ 22 } style={ { marginRight: 2 } } color={ "gray" }/>
-            <Text style={ styles.textGray }>456</Text>
+          <TouchableOpacity style={[styles.flexRow, styles.ml16]}>
+            <EvilIcons name={'retweet'} size={22} style={{marginRight: 2}} color={"gray"}/>
+            <Text style={styles.textGray}>456</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={ [styles.flexRow, styles.ml16] }>
-            <EvilIcons name={ 'heart' } size={ 22 } style={ { marginRight: 2 } } color={ "gray" }/>
-            <Text style={ styles.textGray }>1,456</Text>
+          <TouchableOpacity style={[styles.flexRow, styles.ml16]}>
+            <EvilIcons name={'heart'} size={22} style={{marginRight: 2}} color={"gray"}/>
+            <Text style={styles.textGray}>1,456</Text>
           </TouchableOpacity>
 
-          <TouchableOpacity style={ [styles.flexRow, styles.ml16] }>
-            <EvilIcons name={ Platform.OS === 'ios' ? 'share-apple' : 'share-google' } size={ 22 }
-                       style={ { marginRight: 2 } } color={ "gray" }/>
+          <TouchableOpacity style={[styles.flexRow, styles.ml16]}>
+            <EvilIcons name={Platform.OS === 'ios' ? 'share-apple' : 'share-google'} size={22}
+                       style={{marginRight: 2}} color={"gray"}/>
           </TouchableOpacity>
         </View>
       </View>
@@ -66,16 +84,20 @@ export default function HomeScreen ({ navigation }) {
   const goToNewTweet = () => navigation.navigate("New Tweet")
 
   return (
-    <View style={ styles.container }>
-      <FlatList
-        data={ data }
-        renderItem={ renderItem }
-        keyExtractor={ item => item.id }
-        ItemSeparatorComponent={ () => <View style={ styles.tweetSeparator }></View> }
-      />
-
-      <TouchableOpacity style={ styles.floatingButton } onPress={ () => goToNewTweet() }>
-        <AntDesign name={ 'plus' } size={ 24 } color={ "white" }/>
+    <View style={styles.container}>
+      {isLoading
+        ? (<ActivityIndicator style={{marginTop: 12,}} size="large" color={"gray"}/>)
+        : (<FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+          ItemSeparatorComponent={() => <View style={styles.tweetSeparator}></View>}
+          refreshing={isRefreshing}
+          onRefresh={handleRefresh}
+        />)
+      }
+      <TouchableOpacity style={styles.floatingButton} onPress={() => goToNewTweet()}>
+        <AntDesign name={'plus'} size={24} color={"white"}/>
       </TouchableOpacity>
     </View>
   )
